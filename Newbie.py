78ffad5ee60a963599e68483e3727267a185bf3d
Stafford D'souza <stafford.dsouza@hotmail.com>
2019-07-15 12:20:19 +0400

    Git instructions

--- Newbie.py
+++ Newbie.py
@@ -2,3 +2,13 @@ my_boolean = True
 my_boolean
 
 2==3
+
+"""
+Git steps for committing changes to remote:
+
+git add .
+git commit -m 'My message here'
+git pull origin branchname # here branchname is the branch that you;re working on. This is required when collaborating with team members
+git push origin branchname # remote is updated with local changes
+
+"""
